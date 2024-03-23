556c5b70fd978952acab391e32917167e407ea63
liasovIhsan04 <liasovihsan1@gmail.com>
2024-03-23 15:22:09 +0600

    bakyt start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">ihsan bakyt ihsan</div>;
+  return <div className="App">ihsan bakyt ihsan bakyt</div>;
 }
 
 export default App;
